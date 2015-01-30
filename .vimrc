c360aea17c1a668af275cfdac698c280a447379d
Chris <flynt@oldtech.co>
2015-01-30 07:24:54 -0500

    Add vim-bracketed-paste, small tweaks, install.sh

--- .vimrc
+++ .vimrc
@@ -2,10 +2,14 @@ set tabstop=4 shiftwidth=4 expandtab smartindent
 imap jk <Esc>
 set number
 let mapleader = ","
-let directory="$HOME/.vim/swap"
+set directory=$HOME/.vim/swap/
+
+" pathogen
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
+
+" AirLine
 let g:airline#extensions#tabline#enabled = 1
 let g:airline_theme='badwolf'
 let laststatus=2
@@ -14,5 +18,6 @@ if !exists('g:airline_symbols')
     let g:airline_symbols = {}
 endif
 let g:airline_symbols.space = "\ua0"
+
 colorscheme badwolf
 let badwolf_darkgutter = 1
---
+++ install.sh
@@ -0,0 +1,54 @@
+!/usr/bin/env bash
+
+function dir_exists() {
+  if [ ! -d $1 ]
+    then
+      echo "Target directory $1 does not exist"
+      mkdircmd="mkdir $1"
+      echo $mkdircmd
+      $($mkdircmd)
+  fi        
+}
+
+VIMRC=$HOME/.vimrc
+
+if [ -f $VIMRC ]
+then
+    if [ ! -h $VIMRC ]
+    then
+      echo "$HOME/.vimrc already exists"
+      echo "Backing up $HOME/.vimrc to $HOME/.vim-backup/.vimrc"
+      dir_exists $HOME/.vim-backup
+      mv $HOME/.vimrc $HOME/.vim-backup/.vimrc
+    fi
+fi 
+
+if [ -d $HOME/.vim ]
+then
+    if [ ! -h $VIMRC ]
+    then
+      echo "$HOME/.vim already exists"
+      echo "Backing up $HOME/.vim to $HOME/.vim-backup/.vim"
+      dir_exists $HOME/.vim-backup
+      mv $HOME/.vim $HOME/.vim-backup/.vim
+    fi
+fi 
+
+if [ ! -h $VIMRC ]
+then
+    echo "Creating symbolic links"
+    ln -s $PWD/.vimrc $HOME/.vimrc
+    if [ ! -h $HOME/.vim ]
+    then
+        ln -s $PWD/.vim $HOME/.vim
+    fi
+fi
+
+if [ ! -d $HOME/.fonts ]
+then
+    echo "Installing fonts"
+    $HOME/.vim/fonts/install.sh
+    fc-cache -vf ~/.fonts/
+fi
+
+export TERM=xterm-256color
